9e4a632016965a6a4026a6262e89c396702b80b2
golshan <mehrdad.mohajjel@gmail.com>
2023-12-02 11:07:17 +0330

    Feature Web/program.cs:  feature added

--- Hospital.Web/Program.cs
+++ Hospital.Web/Program.cs
@@ -1,15 +1,19 @@
+using Hospital.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka. ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
