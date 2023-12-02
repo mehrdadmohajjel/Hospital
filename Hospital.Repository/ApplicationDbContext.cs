5a4368d41ac3e7b25f4035b2741c8f7303f72e27
golshan <mehrdad.mohajjel@gmail.com>
2023-12-02 10:59:32 +0330

    Feature Hospital.Repository: All needed nuget added to use ApplicationDbContext

---
+++ Hospital.Repository/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Repository
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+    }
+}
